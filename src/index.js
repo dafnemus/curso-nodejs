4cac52fcbec0a0009199ae63eb8a7b506a031a0a
dafnemus <dafnemus@gmail.com>
2020-05-28 15:09:51 -0300

    importación parcial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var http = require("http");
-
+//var log = require("./module/my-log"); importación glogbal
+var { info, error } = require("./module/my-log");
 var server = http.createServer(function (request, response) {
   //definiendo rutas.
   if (request.url === "/") {
@@ -9,6 +10,16 @@ var server = http.createServer(function (request, response) {
   } else if (request.url === "/exit") {
     response.writeHead(200, { "Conten-Type": "text/html" });
     response.write("<html><body><p>BYE</p></body></html>");
+    response.end();
+  } else if (request.url === "/info") {
+    var result = info(request.url);
+    response.writeHead(200, { "Conten-Type": "text/html" });
+    response.write(result);
+    response.end();
+  } else if (request.url === "/error") {
+    var result = error(request.url);
+    response.writeHead(200, { "Conten-Type": "text/html" });
+    response.write(result);
     response.end();
   } else {
     response.writeHead(404, { "Conten-Type": "text/html" });
