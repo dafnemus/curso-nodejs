d23139c64b31c6a9ad3a75b3266de0dfc8ece705
dafnemus <dafnemus@gmail.com>
2020-05-28 17:34:35 -0300

    modulo desde npm

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 var http = require("http");
-var { info, error } = require("./module/my-log");
+var url = require("url");
+var querystring= require("querystring")
+var { error } = require("./module/my-log");
+var { countries } = require("countries-list");
+
 var server = http.createServer(function (request, response) {
-  //definiendo rutas.
-  if (request.url === "/") {
+  var parsed = url.parse(request.url);
+  console.log("parsed", parsed);
+
+  var pathname = parsed.pathname;
+
+  var query=querystring.parse(parsed.query);
+  console.log("query", query)
+  
+  if (pathname === "/") {
     response.writeHead(200, { "Conten-Type": "text/html" });
     response.write("<html><body><p>HOME PAGE</p></body></html>");
     response.end();
-  } else if (request.url === "/exit") {
+  } else if (pathname === "/exit") {
     response.writeHead(200, { "Conten-Type": "text/html" });
-    response.write(result);
+    response.write("<html><body><p>EXIT</p></body></html>");
     response.end();
-  } else if (request.url === "/info") {
-    var result = info(request.url);
-    response.writeHead(200, { "Conten-Type": "text/html" });
-    response.write(result);
+  } else if (pathname === "/country") {
+    response.writeHead(200, { "Conten-Type": "application/json" });
+    response.write(JSON.stringify(countries[query.code]));
     response.end();
-  } else if (request.url === "/error") {
-    var result = error(request.url);
+  } else if (pathname === "/error") {
+    var result = error(pathname);
     response.writeHead(200, { "Conten-Type": "text/html" });
     response.write(result);
     response.end();
