2a13a5a2443e487c5391c5c400e8659fc578d463
dafnemus <dafnemus@gmail.com>
2020-06-07 18:04:50 -0300

    alternativa a query params

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 const express = require('express');
-const { countries } = require('countries-list');
+const { countries, languages } = require('countries-list');
 const { info, error } = require('./module/my-log');
 
 const app = express();
@@ -16,45 +16,24 @@ app.get('/info', (request, response) => {
 });
 
 app.get('/country', (request, response) => {
-  console.log('request.query', request.query);
+  // console.log('request.query', request.query);
   response.json(countries[request.query.code]);
+});
+
+app.get('/languages/:lang', (request, response) => {
+  // console.log('request.params', request.params);
+  const lang = languages[request.params.lang];
+  if (lang) {
+    response.json({ status: 'OK', data: lang });
+  } else {
+    response.status(404).json({ status: 'Not Found', message: `language ${request.params.lang} not found` });
+  }
 });
 
 app.get('*', (request, response) => {
   response.status(404).send('NOT FOUND');
 });
-// var server = http.createServer(function (request, response) {
-//   var parsed = url.parse(request.url);
-//   console.log("parsed", parsed);
-
-//   var pathname = parsed.pathname;
-
-//   var query = querystring.parse(parsed.query);
-//   console.log("query", query);
-
-//   if (pathname === "/") {
-//     response.writeHead(200, { "Conten-Type": "text/html" });
-//     response.write("<html><body><p>HOME PAGE</p></body></html>");
-//     response.end();
-//   } else if (pathname === "/exit") {
-//     response.writeHead(200, { "Conten-Type": "text/html" });
-//     response.write("<html><body><p>EXIT</p></body></html>");
-//     response.end();
-//   } else if (pathname === "/country") {
-//     response.writeHead(200, { "Conten-Type": "application/json" });
-//     response.end();
-//   } else if (pathname === "/error") {
-//     var result = error(pathname);
-//     response.writeHead(200, { "Conten-Type": "text/html" });
-//     response.write(result);
-//     response.end();
-//   } else {
-//     response.writeHead(404, { "Conten-Type": "text/html" });
-//     response.write("<html><body><p>NOT FOUND</p></body></html>");
-//     response.end();
-//   }
-// });
 
 app.listen(4000, () => {
-  console.log('running on 4000');
+  // console.log('running on 4000');
 });
